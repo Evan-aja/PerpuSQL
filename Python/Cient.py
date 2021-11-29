3f7ad0efc8556f9f7be57504b7d8d6367e326315
Evan-aja <gebel909@studnt.ub.ac.id>
2021-11-30 05:37:33 +0700

    python init

---
+++ Python/Cient.py
@@ -0,0 +1,4 @@
+print('    __  __________    __    ____  __\n   / / / / ____/ /   / /   / __ \/ /\n  / /_/ / __/ / /   / /   / / / / /\n / __  / /___/ /___/ /___/ /_/ /_/\n/_/ /_/_____/_____/_____/\____(_)\n')
+
+print('Apa yang ingan anda lakukan hari ini? (masukkan angka)')
+print('1. List Buku\n2. Pinjam Buku\n3. Ubah Profil')

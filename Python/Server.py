be4a909442f8e67c6c4710fad391b51063a063d5
Evan-aja <gebel909@studnt.ub.ac.id>
2021-12-01 08:07:30 +0700

    some fixes

--- Python/Server.py
+++ Python/Server.py
@@ -19,30 +19,52 @@ serverSocket.bind(('',serverPort))
 
 print('Ready to receive')
 
+def rotate(arr):
+    rt=1
+    for i in range(0, rt):    
+        first = arr[0]
+        for j in range(len(arr)-1):
+            arr[j] = arr[j+1]  
+        arr[len(arr)-1] = first
+    return arr
+
 while True:
     message, clientAddress=serverSocket.recvfrom(2048)
     message=json.loads(message.decode())
     result=pd.read_sql('SELECT 1',cnxn).values
     if(message.get("a")=='1'):
-        result=pd.read_sql("EXEC MASTERBUKU @COMMAND='SELECT'",cnxn).values
+        # result=pd.read_sql("EXEC MASTERBUKU @COMMAND='SELECT'",cnxn).values
+        
+        result=pd.read_sql("SELECT * FROM BUKU ORDER BY ID_BUKU ASC",cnxn).values
     elif(message.get("a")=='2'):
         arr=message.get("b")
-        sql="EXEC MASTERPINJAM @ID_MAHASISWA=?,@ID_BUKU=?,@COMMAND='ADD'"
+        # sql="EXEC MASTERPINJAM @ID_MAHASISWA=?,@ID_BUKU=?,@COMMAND='ADD'"
+        
+        sql="INSERT INTO PINJAM(ID_MAHASISWA,ID_BUKU,TANGGAL_PINJAM,TANGGAL_KEMBALI) VALUES(?,?,GETDATE(),DATEADD(DAY,+8,DATEADD(MILLISECOND,-3,DATEADD(DAY,0, DATEDIFF(DAY,0,getdate())))))"
         cursor.execute(sql,arr)
         cnxn.commit()
-        result=pd.read_sql("EXEC MASTERPINJAM @COMMAND='SELECT'",cnxn)
+        # result=pd.read_sql("EXEC MASTERPINJAM @COMMAND='SELECT'",cnxn)
+
+        # result=pd.read_sql("SELECT * FROM PINJAM ORDER BY ID_PINJAM ASC",cnxn)
         result['TANGGAL_PINJAM']=result['TANGGAL_PINJAM'].astype(str)
         result['TANGGAL_KEMBALI']=result['TANGGAL_KEMBALI'].astype(str)
         result=result.values
     elif(message.get("a")=='3'):
         arr=message.get("b")
         print(arr)
-        sql="EXEC MASTERSISWA @ID_MAHASISWA=?,@NAMA_DEPAN=?,@NAMA_BELAKANG=?,@JENIS_KELAMIN=?,@COMMAND='UPDATE'"
+        # sql="EXEC MASTERSISWA @ID_MAHASISWA=?,@NAMA_DEPAN=?,@NAMA_BELAKANG=?,@JENIS_KELAMIN=?,@COMMAND='UPDATE'"
+        
+        sql="UPDATE MAHASISWA SET NAMA_DEPAN=?,NAMA_BELAKANG=?,JENIS_KELAMIN=? WHERE ID_MAHASISWA=?"
+        arr=rotate(arr)
         cursor.execute(sql,arr)
         cnxn.commit()
-        result=pd.read_sql("EXEC MASTERSISWA @COMMAND='SELECT'",cnxn).values
+        # result=pd.read_sql("EXEC MASTERSISWA @COMMAND='SELECT'",cnxn).values
+        
+        result=pd.read_sql("SELECT * FROM MAHASISWA ORDER BY ID_MAHASISWA ASC",cnxn).values
     elif(message.get("a")=='4'):
-        result=pd.read_sql("EXEC MASTERSISWA @COMMAND='SELECT'",cnxn).values
+        # result=pd.read_sql("EXEC MASTERSISWA @COMMAND='SELECT'",cnxn).values
+        
+        result=pd.read_sql("SELECT * FROM MAHASISWA ORDER BY ID_MAHASISWA ASC",cnxn).values
     print(result)
     returns=json.dumps({"a":result.tolist()})
     serverSocket.sendto(returns.encode(),(clientAddress))
